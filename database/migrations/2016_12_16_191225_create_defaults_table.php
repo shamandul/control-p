5955d2ad32f84e4d828166505d22518d4e05a9ee
Jesús <shamandul@gmail.com>
2016-12-16 20:29:55 +0100

    creado el modelo y la migración de la tabla defaults

---
+++ database/migrations/2016_12_16_191225_create_defaults_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDefaultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('defaults', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tipo' , '150');
+            $table->double('factor' , '5', '2');
+            $table->integer('num_faltas');
+            $table->string('comentario' , '250');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('defaults');
+    }
+}
